c66312733791b33df572e053f7a3db6e64e64f18
Timofey1987 <son_decline@mail.ru>
2024-08-03 10:55:28 +0800

    Add files via upload

--- Module_3_3.py
+++ Module_3_3.py
@@ -4,6 +4,9 @@ def print_params(a = 1, b = 'строка', c = True):
 
 values_list = [2.2, True, [1, 2, 3]]
 values_dict = {'a': 74, 'b': "Ангарск", 'c': 6.5}
+print_params(*values_list)
+print_params(**values_dict)
+
 
 values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42)
